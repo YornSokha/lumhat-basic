ffbfa3fc0c5cb62f03fe7b0111192a75a8acadab
nythyatdora <nythyatdora@gmail.com>
2019-05-19 16:12:03 +0700

    create loadQuestion function

--- prototype/collection.js
+++ prototype/collection.js
@@ -1,6 +1,17 @@
+class Answer {
+
+}
+
+class Question {
+    questionId;
+    questionDescription;
+}
+
 class Collection {
     collectionId;
     collectionName;
-    
-    
+
+    constructor() {
+
+    }
 }
